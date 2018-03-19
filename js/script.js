5655b0a9b95d62e3196773d353d68f92c772fe68
Silver <sardukar.silver@gmail.com>
2018-03-19 23:07:57 +0200

    more fixes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
     var maxFields = 10;
-    var maxRadioFields = 10;
     var wrapper = $(".jsForm");
     var fieldArea = $(".jsFields");
-    var addSnglLine = $(".jsAddSnglLine");
-    var addTextArea = $(".jsAddTextArea");
-    var addRadioList = $(".jsAddRadioList");
-    var addRadioField = $(".jsFields").find(".jsAddRadioField");
-  
+
+//set counter
     var x = 0;
     
 //silgle line
@@ -51,25 +47,25 @@ $(document).ready(function () {
         if (x < maxFields) {
             x++;
             $(fieldArea).append('\
-            <div class="RadioList" data-name="rlst0'+x+'">\
-                <div class= "input-group mb-3" >\
-                <input type="text" class="form-control" placeholder="Field ' + x + '" aria-label="Field ' + x + '" aria-describedby="basic - addon2">\
-            </div>\
-                <div class= "input-group mb-3 jsRadioFIeld">\
-                    <div class="input-group-prepend">\
-                        <div class="input-group-text">\
-                            <input type="radio" aria-label="Radio button for following text input" name="rlst0'+ x +'">\
-                        </div>\
+                <div class="RadioList" data-name="rlst0'+x+'">\
+                    <div class= "input-group mb-3" >\
+                        <input type="text" class="form-control" placeholder="Field ' + x + '" aria-label="Field ' + x + '" aria-describedby="basic - addon2">\
                     </div>\
-                    <input type="text" class="form-control" aria-label="Text input with radio button">\
-                        <div class="input-group-append">\
-                            <button class="btn btn-outline-secondary jsAddRadioField" type="button">\
-                                <i class="fa fa-plus-circle"></i>\
-                            </button>\
-                            <button class="btn btn-outline-secondary jsDeleteRadio" type="button">\
-                                <i class="fa fa-trash-alt"></i>\
-                            </button>\
-                        </div>\
+                <div class= "input-group mb-3 jsRadioFIeld">\
+                <div class="input-group-prepend">\
+                    <div class="input-group-text">\
+                        <input type="radio" aria-label="Radio button for following text input" name="rlst0'+ x +'">\
+                    </div>\
+                </div>\
+                <input type="text" class="form-control" aria-label="Text input with radio button">\
+                    <div class="input-group-append">\
+                        <button class="btn btn-outline-secondary jsAddRadioField" type="button">\
+                            <i class="fa fa-plus-circle"></i>\
+                        </button>\
+                        <button class="btn btn-outline-secondary jsDeleteRadio" type="button">\
+                            <i class="fa fa-trash-alt"></i>\
+                        </button>\
+                    </div>\
                 </div>\
             </div>');}
     });
@@ -112,7 +108,7 @@ $(document).ready(function () {
         if (!num) {
             radioList.remove();
         }
-    })
+    });
 
 
 
